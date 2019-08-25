a99f3456f4ec3f86664bcbe5f5ff38addf7e291f
Nathan Hollows <me@nathanhollows.com>
2019-08-25 13:28:18 +1200

    Install composer and fetch libraries

--- installs/build_webserver.sh
+++ installs/build_webserver.sh
@@ -1,8 +1,17 @@
 add-apt-repository ppa:ondrej/php
-apt update
-apt install -y apache2 php libapache2-mod-php php-msql php-curl php-fileinfo imagemagick php-imagick
-apt install php-phalcon
-cp /vagrant/nugget.conf /etc/apache2/sites-available/
-a2ensite nugget
-a2disstite 000-default
-service apache2 reload
+apt-get update
+apt-get install -y apache2 php libapache2-mod-php php-mysql php-curl imagemagick php-imagick
+apt-get install php-phalcon
+cp /vagrant/installs/000-default.conf /etc/apache2/sites-available/000-default.conf
+a2enmod rewrite
+service apache2 restart
+
+# install composer
+cd ~
+curl -sS https://getcomposer.org/installer -o composer-setup.php
+php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+cd /vagrant/www/
+# This command is going to look a lot worse that it is.
+# Composer installs a php library for formatting time in a human readable way
+# The entire output will be red, but in this case it's fine
+composer install
