057dce8eece5463ec5a22f573d56867a41c9fdb0
Nathan Hollows <me@nathanhollows.com>
2019-08-30 13:26:26 +1200

    Toggle status of other featured reviews for this chain

--- www/app/controllers/ReviewsController.php
+++ www/app/controllers/ReviewsController.php
@@ -36,16 +36,27 @@ class ReviewsController extends ControllerBase {
 
     public function featureAction($id) {
         $review = Reviews::findFirstByWebid($id);
+        // check if the reviews exists
         if (!$review) {
             $this->flashSession->error("The review could not be found");
             return $this->response->redirect("reviews");
         }
+
+        // Toggle featured
         if ($review->featured == 1) {
             $review->featured = 0;
         } else {
+            // Check if another review is featured
+            $old = Reviews::findFirst(["featured = 1 AND chain = $review->chain"]);
+            // Remove he featured flag
+            if ($old) {
+                $old->featured = 0;
+                $old->save();
+            };
             $review->featured = 1;
         }
 
+        // Check if the review has been successfully saved
         if ($review->save()) {
             $this->flashSession->success("Featured review set successfullly");
             return $this->response->redirect("reviews");
